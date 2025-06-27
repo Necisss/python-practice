4a2798c9d4cfa345c6184eddc87a284306f9d81d
Natalia Plaza <nataliaplazajorquera@gmail.com>
2025-06-27 23:09:40 +0800

    More operator exercises

---
+++ lesson_operators_holypython.py
@@ -0,0 +1,14 @@
+# Exercise 18-a: Assignment Operator (=)
+lst = ["yellow", "white", "blue"]
+print(lst)  # Output: ['yellow', 'white', 'blue']
+
+# Exercise 18-b: Division Operator (/)
+a = 10
+b = 3
+result = a / b
+print(result)  # Output: 3.333...
+
+# Exercise 18-c: Addition using +=
+vertical_speed = 750
+vertical_speed += 100
+print(vertical_speed)  # Output: 850
