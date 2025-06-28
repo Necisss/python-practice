639bfe8ce9662432c7df3580c10caac0352d52bb
Natalia Plaza <nataliaplazajorquera@gmail.com>
2025-06-28 09:39:10 +0800

    Add more regex pattern examples to Activity 2.4

--- activity_2_4_regex_practice.py
+++ activity_2_4_regex_practice.py
@@ -47,3 +47,33 @@ print(x.string)  # Output: The rain in Spain
 # Print the part of the string that matched
 print(x.group())  # Output: Spain
 
+import re
+
+# Match a specific sequence '123'
+s = 'foo123bar'
+x = re.search('123', s)
+print(x)  # Output: <re.Match object; span=(3, 6), match='123'>
+
+# Conditional match check
+if re.search('123', s):
+    print('Found a match.')
+else:
+    print('No match.')
+
+# Match three consecutive digits
+print(re.search('[0-9][0-9][0-9]', 'foo123bar'))  # Output: <re.Match object; span=(3, 6), match='123'>
+print(re.search('[0-9][0-9][0-9]', 'foo456bar'))  # Output: <re.Match object; span=(3, 6), match='456'>
+print(re.search('[0-9][0-9][0-9]', '234baz'))     # Output: <re.Match object; span=(0, 3), match='234'>
+print(re.search('[0-9][0-9][0-9]', 'qux678'))     # Output: <re.Match object; span=(3, 6), match='678'>
+print(re.search('[0-9][0-9][0-9]', '12foo34'))    # Output: None
+
+# Match '1' followed by any character, then '3'
+s = 'foo123bar'
+print(re.search('1.3', s))  # Output: <re.Match object; span=(3, 6), match='123'>
+
+s = 'foo13bar'
+print(re.search('1.3', s))  # Output: None
+
+# Reconfirm exact match '123'
+s = 'foo123bar'
+print(re.search('123', s))  # Output: <re.Match object; span=(3, 6), match='123'>
